1b5d37c7091c34a1a2ccf03eba4fca6e65cd0c67
merranleo <leo.merran@ext.bpifrance.fr>
2024-10-15 22:58:29 +0200

    feat: implement search logic

--- dojo-cs-cemantics/src/app/app.component.ts
+++ dojo-cs-cemantics/src/app/app.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormsModule} from "@angular/forms";
+import {ApiClientService} from "./service/api-client.service";
+import {HttpClientModule} from "@angular/common/http";
+import {InputTextModule} from "primeng/inputtext";
+import {Button} from "primeng/button";
+import {ProgressBarModule} from "primeng/progressbar";
+import {catchError, EMPTY} from "rxjs";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, HttpClientModule, InputTextModule, Button, ProgressBarModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [ApiClientService]
 })
 export class AppComponent {
-  title = 'dojo-cs-cemantics';
+  private apiClient = inject(ApiClientService);
+
+  myGuess: string = "";
+  hasError = false;
+  myResultDistance = 0
+
+  sendMyGuess() {
+    this.myResultDistance = 45
+    this.apiClient.getPokemonValue({
+      value: this.myGuess
+    }).pipe(
+      catchError(err => {
+        this.myResultDistance = 0
+        this.hasError = true;
+        return EMPTY
+      })
+    ).subscribe(result=> {
+      this.hasError = false;
+      this.myResultDistance = result.value
+    });
+  }
 }
--- dojo-cs-cemantics/src/app/service/api-client.service.ts
+++ dojo-cs-cemantics/src/app/service/api-client.service.ts
@@ -2,6 +2,7 @@ import {inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {ResponseWord} from "../model/responseWord.model";
+import {GuessWord} from "../model/guessWord.model";
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ import {ResponseWord} from "../model/responseWord.model";
 export class ApiClientService {
   private httpClient = inject(HttpClient);
 
-  public getPokemonValue(guess: string): Observable<ResponseWord>{
+  public getPokemonValue(guess: GuessWord): Observable<ResponseWord>{
     return this.httpClient.get<ResponseWord>(`https://pokeapi.co/api/v2/pokemon/${guess}`)
   }
 }
